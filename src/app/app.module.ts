436d03b60abf562389c2eda2e5e0140bc9d4675c
Joyce Cho <joyce.cho@colorado.edu>
2018-07-25 11:59:55 -0600

    Add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { BodyComponent } from './body/body.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import 'hammerjs';
@@ -14,16 +13,17 @@ import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { PhotosComponent } from './photos/photos.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    BodyComponent,
     AboutComponent,
     ProjectsComponent,
     PhotosComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routing/routes.ts
+++ src/app/routing/routes.ts
@@ -3,8 +3,10 @@ import { AboutComponent } from '../about/about.component';
 import {ProjectsComponent} from '../projects/projects.component';
 import {PhotosComponent} from '../photos/photos.component';
 import {ContactComponent} from '../contact/contact.component';
+import {HomeComponent} from '../home/home.component';
 
 export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'photos', component: PhotosComponent },
