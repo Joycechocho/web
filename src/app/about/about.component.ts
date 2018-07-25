cc87823166b08c9f047766178b8b1dff7c59ef84
Joyce Cho <joyce.cho@colorado.edu>
2018-07-25 16:34:47 -0600

    Add Home and About page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  chart = [];
   constructor() { }
 
   ngOnInit() {
+    this.chart = new Chart(document.getElementById("bar-chart-horizontal"), {
+      type: 'horizontalBar',
+      data: {
+        labels: ["Angular", "Javascript", "Embedded System", "Web Development", "Java"],
+        datasets: [
+          {
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            data: [2478,5267,734,784,433]
+          }
+        ]
+      },
+      options: {
+        legend: { display: false },
+        title: {
+          display: true,
+          text: ''
+        }
+      }
+    });
   }
 
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
+  pictures;
   constructor() { }
 
   ngOnInit() {
+    this.pictures = [
+      {'url': '/assets/images/1.JPG',
+        'title': 'Aliquam erat volutpat',
+        'caption': 'imperdiet imperdiet. Nullam ut ligula vitae arcu vulputate dictum ut quis elit.'},
+      {'url': '/assets/images/2.JPG',
+        'title': 'Aliquam erat volutpat',
+        'caption': 'imperdiet imperdiet. Nullam ut ligula vitae arcu vulputate dictum ut quis elit.'},
+      {'url': '/assets/images/3.JPG',
+        'title': 'Aliquam erat volutpat',
+        'caption': 'imperdiet imperdiet. Nullam ut ligula vitae arcu vulputate dictum ut quis elit.'},
+      {'url': '/assets/images/4.JPG',
+        'title': 'Aliquam erat volutpat',
+        'caption': 'imperdiet imperdiet. Nullam ut ligula vitae arcu vulputate dictum ut quis elit.'},
+      {'url': '/assets/images/5.JPG',
+        'title': 'Aliquam erat volutpat',
+        'caption': 'imperdiet imperdiet. Nullam ut ligula vitae arcu vulputate dictum ut quis elit.'}
+    ];
   }
 
 }
